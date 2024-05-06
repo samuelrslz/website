ea596d9eee0d4f271229ef65efd061247a55eb5f
samuelrslz <rioslazosamuel@gmail.com>
2024-05-06 14:24:02 -0600

    typer effect

---
+++ scripts/type.js
@@ -0,0 +1,18 @@
+var i = 0;
+var txt = "Hi, I'm Samuel Rios-Lazo";
+var speed = 100;
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("demo").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  } else {
+    // Add cursor when typing is finished
+    document.getElementById("cursor").style.display = "inline";
+  }
+}
+
+window.onload = function () {
+  typeWriter();
+};
